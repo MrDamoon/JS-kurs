f3dd066e6ee44e218bd3ea556143b2a8b327dee9
MrDamoon <mrdakase2@gmail.com>
2022-10-05 20:00:14 +0200

    Zadanie 0,5

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+const country = "Poland";
+const continent = "Europe";
+let population = "1";
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+let language;
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+language = "Polish";
