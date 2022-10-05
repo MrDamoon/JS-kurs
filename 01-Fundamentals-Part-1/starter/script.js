4c3f3b84c564c41c1beef9711c4a56362b81323d
MrDamoon <mrdakase2@gmail.com>
2022-10-06 00:49:30 +0200

    Coding Challenge #1 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,15 +1,42 @@
-const country = "Poland";
-const continent = "Europe";
-let population = "1";
-console.log(country);
-console.log(continent);
-console.log(population);
+// const country = "Poland";
+// const continent = "Europe";
+// let population = 36;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-const isIsland = false;
-let language;
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language);
+// const isIsland = false;
+// let language;
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
 
-language = "Polish";
+// language = "Polish";
+
+// console.log(population / 2);
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// let description =
+//   country +
+//   " is in " +
+//   continent +
+//   ", and its " +
+//   population +
+//   " milions people speaking " +
+//   language;
+
+// console.log(description);
+
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
