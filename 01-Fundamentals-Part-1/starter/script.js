700624c373a167e39e721f72ff22b4bfb929202d
MrDamoon <mrdakase2@gmail.com>
2022-10-10 02:32:26 +0200

    Lecture 21 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,36 +1,14 @@
 const country = "Poland";
 const continent = "Europe";
 let population = 36;
-// console.log(country);
-// console.log(continent);
-// console.log(population);
-
 const isIsland = false;
 let language;
-// console.log(isIsland);
-// console.log(population);
-// console.log(country);
-// console.log(language);
-
 language = "Polish";
-
-// console.log(population / 2);
-// console.log(population);
-// console.log(population > 6);
-// console.log(population < 33);
-// let description =
-//   country +
-//   " is in " +
-//   continent +
-//   ", and its " +
-//   population +
-//   " milions people speaking " +
-//   language;
-
 const description = `${country} is in ${continent}, and its ${population} milions people speaking ${language}`;
 
-console.log(description);
+// console.log(description);
 
+//Coding challenge #1
 // let markWeight = 78;
 // let markHeight = 1.69;
 // let johnWeight = 92;
@@ -41,4 +19,29 @@ console.log(description);
 
 // let markHigherBMI = markBMI > johnBMI;
 
-// console.log(markBMI, johnBMI, markHigherBMI);
+//console.log(markBMI, johnBMI, markHigherBMI);
+
+//Coding challenge #2
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI (${markBMI} is higher than John's (${johnBMI})`);
+} else {
+  console.log(`John's BMI (${johnBMI} is higher than Mark's (${markBMI})`);
+}
+
+// if (population > 33) {
+//   console.log(`${country}'s population is above average`);
+// } else {
+//   console.log(
+//     `${country}'s population is ${33 - population} million below average`
+//   );
+// }
