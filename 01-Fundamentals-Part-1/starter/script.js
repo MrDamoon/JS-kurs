989b5c80021bd7e0ae45d11124a300e07bdcad04
MrDamoon <mrdakase2@gmail.com>
2022-10-10 20:02:59 +0200

    Class 23 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -19,24 +19,22 @@ const description = `${country} is in ${continent}, and its ${population} milion
 
 // let markHigherBMI = markBMI > johnBMI;
 
-//console.log(markBMI, johnBMI, markHigherBMI);
-
 //Coding challenge #2
-let markWeight = 78;
-let markHeight = 1.69;
-let johnWeight = 92;
-let johnHeight = 1.95;
+// let markWeight = 78;
+// let markHeight = 1.69;
+// let johnWeight = 92;
+// let johnHeight = 1.95;
 
-let markBMI = markWeight / markHeight ** 2;
-let johnBMI = johnWeight / johnHeight ** 2;
+// let markBMI = markWeight / markHeight ** 2;
+// let johnBMI = johnWeight / johnHeight ** 2;
 
-let markHigherBMI = markBMI > johnBMI;
+// let markHigherBMI = markBMI > johnBMI;
 
-if (markBMI > johnBMI) {
-  console.log(`Mark's BMI (${markBMI} is higher than John's (${johnBMI})`);
-} else {
-  console.log(`John's BMI (${johnBMI} is higher than Mark's (${markBMI})`);
-}
+// if (markBMI > johnBMI) {
+//   console.log(`Mark's BMI (${markBMI} is higher than John's (${johnBMI})`);
+// } else {
+//   console.log(`John's BMI (${johnBMI} is higher than Mark's (${markBMI})`);
+// }
 
 // if (population > 33) {
 //   console.log(`${country}'s population is above average`);
@@ -45,3 +43,17 @@ if (markBMI > johnBMI) {
 //     `${country}'s population is ${33 - population} million below average`
 //   );
 // }
+
+// const numNeighbours = Number(
+//   prompt(`How many neighbour countries does your country have?`)
+// );
+// if (numNeighbours === 1) {
+//   console.log(`Only 1 border`);
+// } else if (numNeighbours > 1) {
+//   console.log(`More than 1 border`);
+// } else {
+//   console.log(`No borders`);
+// }
+
+if (language === "English" && population < 50 && isIsland === false) {
+}
